096adfb1933318ed4aef6fd7e5c2e8ac88d0bfaf
finleypeng <weilllyox1020@gmail.com>
2023-01-12 10:55:20 +0800

    feat: 將 todoList CRUD 功能串接 fireStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDom from "react-dom/client";
 import App from "./components/App";
+// Import the functions you need from the SDKs you need
+// import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+	authDomain: "practice-9ef70.firebaseapp.com",
+	projectId: "practice-9ef70",
+	storageBucket: "practice-9ef70.appspot.com",
+	messagingSenderId: "396093142256",
+	appId: "1:396093142256:web:1b6e45d30550059b2dfe35",
+	measurementId: "G-0NHZD03EZV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<App />);
